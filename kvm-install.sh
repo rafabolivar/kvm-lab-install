bd5b00a833ac7638cfe68667e2cf986af326d000
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-02-21 22:15:06 +0100

    Update kvm-install.sh

--- kvm-install.sh
+++ kvm-install.sh
@@ -23,6 +23,58 @@ sudo systemctl status libvirtd
 sudo usermod -aG kvm $USER
 sudo usermod -aG libvirt $USER
 
+echo "Configuring Networking"
+echo "----------------------"
+
+cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.orig
+echo "network:
+  ethernets:
+    eno1:
+      dhcp4: false
+      dhcp6: false
+
+  bridges:
+    br0:
+      interfaces: [eno1]
+      addresses:
+      - 192.168.1.30/24
+      mtu: 1500
+      nameservers:
+        addresses:
+        - 1.1.1.1
+        - 8.8.8.8
+        search: []
+      routes:
+      - to: default
+        via: 192.168.1.1
+  version: 2" | sudo tee /etc/netplan/00-installer-config.yaml
+sudo netplan generate
+sudo netplan --debug apply
+# bridge control
+brctl show
+# network control
+networkctl
+networkctl status br0
+# ip list
+ip a show br0
+# show host routes
+ip route
+# show arp table (IP to MAC)
+arp -n
+
+echo "<network>
+  <name>host-bridge</name>
+  <forward mode="bridge"/>
+  <bridge name="br0"/>
+</network>" | sudo tee /tmp/host-bridge.xml
+# create libvirt network using existing host bridge
+virsh net-define /tmp/host-bridge.xml
+virsh net-start host-bridge
+virsh net-autostart host-bridge
+
+# state should be active, autostart, and persistent
+virsh net-list --all
+
 echo "Installing Required Packages for Cockpit"
 echo "----------------------------------------"
 
